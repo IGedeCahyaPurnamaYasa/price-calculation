33a7bc173b4d3ff98c79cedcd4e1cd568fe2e260
CahyaPurnama <33152658+IGedeCahyaPurnamaYasa@users.noreply.github.com>
2022-05-30 22:35:38 +0800

    when root updated all child updated

--- controllers/root_ingridients.js
+++ controllers/root_ingridients.js
@@ -1,6 +1,5 @@
 const RootIngridient = require('../models/root_ingridient');
 const Ingridient = require('../models/ingridient');
-const { findByIdAndUpdate } = require('../models/root_ingridient');
 
 const title = 'Ingridient';
 
@@ -54,7 +53,7 @@ module.exports.update = async (req, res) => {
     const ingridient  = await RootIngridient.findByIdAndUpdate(id, {...req.body.ingridient});
 
     await ingridient.save();
-    await re_calculate_ingridient_product(ingridient);
+    await re_calculate_ingridient_product(ingridient._id, req.body.ingridient);
 
     req.flash('success', 'Successfully updated ingridient!');
     res.redirect(`/ingridient`);
@@ -65,7 +64,6 @@ module.exports.delete = async (req, res) => {
     const { id } = req.params;
 
     const ingridient = await Ingridient.find({root_ingridient_id: id});
-    console.log(ingridient);
     if(ingridient.length > 0){
         req.flash('error', 'Cannot delete this root ingridient, already used on product');
         res.redirect('/ingridient');
@@ -77,13 +75,11 @@ module.exports.delete = async (req, res) => {
     res.redirect('/ingridient');
 }
 
-const re_calculate_ingridient_product = async (ingridient) => {
-    const product_ingridients = await Ingridient.find({root_ingridient_id: ingridient.id});
-
+const re_calculate_ingridient_product = async (ingridient_id, ingridient) => {
+    const product_ingridients = await Ingridient.find({root_ingridient_id: ingridient_id});
     for(let temp of product_ingridients){
-        await Ingridient.findByIdAndUpdate(temp._id, {
-            price: ingridient.price,
-            total: temp.qty * ingridient.price
-        });
+        temp.price = ingridient.price
+        temp.total = temp.qty * ingridient.price
+        await temp.save();
     }
 }
